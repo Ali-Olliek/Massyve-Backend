f2324f5b82c2ca7b3d0951090ffa7e1425d2f512
Ali-Olliek <Olliek95@gmail.com>
2024-11-02 00:13:29 +0200

    Adjusted UserId to Accept Being an ObjectId

--- src/utils/classes/Request.ts
+++ src/utils/classes/Request.ts
@@ -1,10 +1,10 @@
 import { Request } from 'express';
-import { JwtPayload } from 'jsonwebtoken';
+import { ObjectId } from 'mongoose';
 
 // Extend the Base `Request` Class
 // I want to pass the userId and token (or any claims necessary) after passing the Authmiddleware to not to re-verify the token
 export interface AuthenticatedRequest extends Request {
-  userId?: string | JwtPayload;
+  userId?: string | ObjectId;
   token?: string;
 };
 
