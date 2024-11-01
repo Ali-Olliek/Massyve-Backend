14bc15078e6551c91521f004093041964ce1e88b
Ali-Olliek <Olliek95@gmail.com>
2024-11-02 00:14:31 +0200

    Minor Adjustments to Authentication Middleware

--- src/middlewares/isAuthenticated.middleware.ts
+++ src/middlewares/isAuthenticated.middleware.ts
@@ -2,23 +2,29 @@ import jwt from "jsonwebtoken"
 import { Response, NextFunction} from "express"
 import JWTConfig from "../configs/jwt"
 import type { AuthenticatedRequest } from "../utils/classes/Request"
+import { IPayloadReq, IPayloadRes } from '../services/authentication.service';
 
-const isAuthenticated = async (req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<any> => {
-    const token = req.headers.authorization?.split(" ")[1];
+const isAuthenticated = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  const token = req.headers.authorization?.split(' ')[1];
 
-    if (!token) return res.status(401).json({message: "Access Denied", error: true})
+  if (!token)
+    return res.status(401).json({ message: 'Access Denied', error: true });
 
-    try {
-        const decoded = jwt.verify(token, JWTConfig.key)
+  try {
+    const decoded = jwt.verify(token, JWTConfig.key) as IPayloadRes;
 
-        req.userId = decoded
-        req.token = token
+    req.userId = decoded.userId;
+    req.token = token;
 
-        next()
-    } catch (error) {
-        console.error(error)
-        res.status(401).json({ message: 'Invalid Token', error: true });
-    }
-}
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: 'Invalid Token', error: true });
+  }
+};
 
 export default isAuthenticated
