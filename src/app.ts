3983f2959cf5b2f41a1a11d722195c8a4ce2dff5
Ali-Olliek <Olliek95@gmail.com>
2024-11-01 22:43:10 +0200

    Created Main App Entry

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express"
+import bodyParser from "body-parser"
+import { selectedConfig } from "./configs"
+import authenticationRoutes from "./routes/authentication"
+import mongoose from "mongoose"
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use("/api/auth", authenticationRoutes)
+
+mongoose
+.connect(selectedConfig.dbConnectionString).then(() => console.log("Connected to MongoDB Atlas"))
+.catch(error => console.error("Error connecting to MongoDB Atlas", error))
+
+
+app.listen(selectedConfig.port, ()=> {
+    console.log(`Example app listening to http://localhost:${selectedConfig.port}`)
+})
