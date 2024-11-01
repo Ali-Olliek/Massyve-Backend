11cf652293d18141916adec65b67952392bc62f0
Ali-Olliek <Olliek95@gmail.com>
2024-11-02 00:14:54 +0200

    Uncovered the class of User outside the Mongoose Schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,21 @@
 import mongoose, {Schema} from "mongoose";
 
-const UserSchema = new Schema({
-    username: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    refreshToken: {type: String, required: false}
-})
+export interface IUser extends mongoose.Document {
+  username: string;
+  email: string;
+  password: string;
+  refreshToken: string;
+}
 
-const UserModel = mongoose.model("User", UserSchema)
+const UserSchema = new Schema<IUser>({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  refreshToken: { type: String, required: false },
+});
 
-export default UserModel;
+export interface IUserModel extends mongoose.Model<IUser> {} // https://stackoverflow.com/a/37970346
+
+export const UserModel: IUserModel = <IUserModel>(
+  mongoose.model<IUser>('User', UserSchema)
+);
